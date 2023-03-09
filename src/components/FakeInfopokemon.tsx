33ebe4ebed6534bef09adccc4707e308dbe8d849
akarawit <kooglell7410@gmail.com>
2023-03-09 12:29:00 +0700

    update alt in img tag

--- src/components/FakeInfopokemon.tsx
+++ src/components/FakeInfopokemon.tsx
@@ -6,6 +6,7 @@ const FakeInfopokemon: React.FC = () => {
       <img
         className="relative w-40 h-40 object-contain top-10 "
         src="https://sv1.picz.in.th/images/2022/11/18/G41hJt.png"
+        alt="fakepokemon"
       />
       <div className="flex-grow border rounded-sm flex justify-center items-center w-full">
         <h4>
